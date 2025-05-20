4f15be0eb88249a172092ab345c2445f58f3ec7b
muskan <muskanbharatkesharwani@gmail.com>
2025-05-20 23:21:48 +0530

    Initial commit: Project setup and DB config

---
+++ src/test/java/com/realestate/propertyapp/PropertyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realestate.propertyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
